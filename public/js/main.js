245d5b5af5ce3c23c114058489ec5c8794d309ff
cyril.haubourdin@gmail.com <cyril.haubourdin@gmail.com>
2020-09-07 10:29:30 +0200

    test

---
+++ public/js/main.js
@@ -0,0 +1,108 @@
+//EXO 1 : FAIT
+
+//EXO 2 : FAIT
+//let maBoolean = true
+
+
+//EXO 3 : FAIT
+//let monArray = []
+
+
+//EXO 4 : FAIT
+// let monObjet = {
+//     prenom : Cyril
+// }
+
+
+//EXO 5 : FAIT
+//let monString = "cyril"
+
+
+//EXO 6 : 
+
+
+//EXO 7 : FAIT
+// let monObjet = {
+//     x(){
+
+//     }
+// }
+
+
+//EXO 8 : FAIT
+// function cyril(x, y) {
+    
+// }
+
+
+//EXO 9 : FAIT
+// let monInt = 7
+// if (monInt < 8) {
+    
+// }
+
+
+//EXO 10 : FAIT
+// for (let i = 0; i < 9, i++) {
+    
+    
+// }
+
+// EXO 11 : FAIT
+// let date = new Date('September 9, 2020')
+
+
+//EXO 12 : 
+// let mot = "string"
+// let mot2 = mot.toUpperCase()
+
+
+//EXO 13 :
+
+
+
+//EXO 14 :
+// let mot = "cyril"
+// alert(mot.length)
+
+
+//EXO 15 :
+
+//EXO 16 :
+
+//EXO 17 :
+
+//EXO 18 :
+let objet = {
+    nom : "",
+    age : "",
+    modeling()
+}
+
+//EXO 19 :
+
+//EXO 20 :
+let carre = {
+    size : "" ,
+    borderaspect : "",
+    epaisseur : "" ,
+    couleur : "",
+    ajouter(){
+        if (this.borderaspect !== "solid") {
+            alert("Incorrect. Veuillez réessayer.")
+        } else if (this.borderaspect !== "dotted") {
+            alert("Incorrect. Veuillez réessayer.")
+        } else if (this.couleur !== "green"){
+            alert("Incorrect. Veuillez réessayer.")
+        } else if (this.couleur !== "red"){
+            alert("Incorrect. Veuillez réessayer.")
+        } else if (this.couleur !== "blue"){
+            alert("Incorrect. Veuillez réessayer.")
+        }
+            
+    }   
+}
+
+
+
+
